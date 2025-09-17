68eb58557d7f2f29f7e3b6b5069daaf70d11b343
kojing777 <kojingmoktan92@gmail.com>
2025-09-17 23:38:10 +0545

    restructure App component and clean up index.css imports

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import ChatBox from "./components/ChatBox";
+import Credits from "./pages/Credits";
+import Community from "./pages/Community";
 
 const App = () => {
   return (
-    <div className="bg-blue-500 text-white p-4">kojing</div>
-  )
-}
+    <>
+      <div>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<ChatBox />} />
+          <Route path="/credits" element={<Credits />} />
+          <Route path="/community" element={<Community />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
