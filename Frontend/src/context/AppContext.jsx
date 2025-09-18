64bf387df3f853ffe6431f9b690dd927f19f9609
kojing777 <kojingmoktan92@gmail.com>
2025-09-18 11:28:34 +0545

    add AppContextProvider for global state management and user context

--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -1,9 +1,17 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-    const value = {};
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [chats, setChats] = useState([]);
+    const [selectedChat, setSelectedChat] = useState(null);
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+    const value = { user, setUser, navigate, chats, setChats, selectedChat, setSelectedChat, theme, setTheme };
     return(
         <AppContext.Provider value={value}>
             {children}
