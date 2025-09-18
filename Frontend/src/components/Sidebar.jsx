922f9c7440a901ecd570953a06728718232c2e8e
kojing777 <kojingmoktan92@gmail.com>
2025-09-18 12:53:05 +0545

    Sidebar and AppContext for improved state management and user chat fetching

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 const Sidebar = () => {
+  const { chats, theme, setSelectedChat, setTheme, user } = useAppContext();
+  const [search, setSearch] = useState("");
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609f]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1">
+      {/* logo */}
+      <img
+        src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
+        alt=""
+        className="w-full max-w-48"
+      />
+      {/* new chat button  */}
+      <button className="flex justify-center items-center w-full py-2 mt-10 text-white bg-gradient-to-r from-[#a456f7] to-[#3d81f6] text-sm rounded-md cursor-pointer">
+        <span className="mr-2 text-xl">+</span> New Chat
+      </button>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { dummyUserData } from "./../assets/assets";
+import { dummyChats, dummyUserData } from "./../assets/assets";
 
 const AppContext = createContext();
 
@@ -14,6 +14,27 @@ export const AppContextProvider = ({ children }) => {
   const fetchUser = async () => {
     setUser(dummyUserData);
   };
+
+  const fetchUsersChats = async () => {
+    setChats(dummyChats)
+    setSelectedChat(dummyChats[0]);
+  };
+useEffect(() => {
+    if (user) {
+      fetchUsersChats();
+    }else {
+      setChats([]);
+      setSelectedChat(null);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
 
   useEffect(() => {
     fetchUser();
