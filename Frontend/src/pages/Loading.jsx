3541f89598406ad96b42d9847a15a463711c3676
kojing777 <kojingmoktan92@gmail.com>
2025-09-18 23:23:40 +0545

    fix formatting and consistency in Loading component

--- Frontend/src/pages/Loading.jsx
+++ Frontend/src/pages/Loading.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
   return (
-    <div className='bg-gradient-to-b from-[#531b81] to-[#29184b] backdrop-capacity-60 flex items-center justify-center h-screen w-screen text-white text-2xl'>
-
+    <div className="bg-gradient-to-b from-[#531b81] to-[#29184b] backdrop-capacity-60 flex items-center justify-center h-screen w-screen text-white text-2xl">
+      <div className="w-10 h-10 rounded-full border-3 border-white border-t-transparent animate-spin"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
