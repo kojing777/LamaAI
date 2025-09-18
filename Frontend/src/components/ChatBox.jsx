be90fe5d0ecf0bcee81ef0fefd56c71a3114c8da
kojing777 <kojingmoktan92@gmail.com>
2025-09-18 22:25:05 +0545

    ChatBox component for improved state management and layout

--- Frontend/src/components/ChatBox.jsx
+++ Frontend/src/components/ChatBox.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useAppContext } from "../context/appContext";
+import { assets } from "../assets/assets";
 
 const ChatBox = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { selectedChat, theme } = useAppContext();
 
-export default ChatBox
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (selectedChat) {
+      setMessages(selectedChat.messages);
+    }
+  }, [selectedChat]);
+
+  return (
+    <div className="flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40">
+      {/* chat messages */}
+      <div className="flex-1 overflow-y-scroll">
+        {messages.length === 0 && (
+          <div className="h-full flex flex-col items-center justify-center gap-2 text-primary">
+            <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} className="w-full max-w-56 sm:max-w-68" alt="" />
+            <p className="mt-5 text-4xl sm:text-6xl text-center text-gray-400 dark:text-white">Ask Me Anything</p>
+          </div>
+        )}
+      </div>
+      {/* prompt input box */}
+      <form action="">
+        
+      </form>
+    </div>
+    // prompt input area
+  );
+};
+
+export default ChatBox;
