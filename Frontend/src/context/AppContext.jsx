1ff6bd1e639b06cf4a56f6be08ac3a0ea37029e8
kojing777 <kojingmoktan92@gmail.com>
2025-09-18 11:50:35 +0545

    AppContextProvider to include user fetching logic and improve state management

--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -1,22 +1,40 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyUserData } from "./../assets/assets";
 
 const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [chats, setChats] = useState([]);
+  const [selectedChat, setSelectedChat] = useState(null);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
-    const navigate = useNavigate();
-    const [user, setUser] = useState(null);
-    const [chats, setChats] = useState([]);
-    const [selectedChat, setSelectedChat] = useState(null);
-    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const fetchUser = async () => {
+    setUser(dummyUserData);
+  };
 
-    const value = { user, setUser, navigate, chats, setChats, selectedChat, setSelectedChat, theme, setTheme };
-    return(
-        <AppContext.Provider value={value}>
-            {children}
-        </AppContext.Provider>
-    )
-}
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const value = {
+    user,
+    setUser,
+    navigate,
+    chats,
+    setChats,
+    selectedChat,
+    setSelectedChat,
+    theme,
+    setTheme,
+  };
+  return (
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  );
+};
 
 export const useAppContext = () => useContext(AppContext);
