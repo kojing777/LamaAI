d15fdf6c92f0767756e896b846bdbe0415271d70
kojing777 <kojingmoktan92@gmail.com>
2025-09-18 23:22:42 +0545

    enhance Loading component with gradient background and centering styles

--- Frontend/src/pages/Loading.jsx
+++ Frontend/src/pages/Loading.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <div className='bg-gradient-to-b from-[#531b81] to-[#29184b] backdrop-capacity-60 flex items-center justify-center h-screen w-screen text-white text-2xl'>
+
+    </div>
   )
 }
 
