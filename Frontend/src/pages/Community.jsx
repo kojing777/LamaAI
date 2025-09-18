73527efadb1cfa1b4bce6b40ac88f3a8765acc3b
kojing777 <kojingmoktan92@gmail.com>
2025-09-18 23:41:04 +0545

    add image loading functionality in Community component

--- Frontend/src/pages/Community.jsx
+++ Frontend/src/pages/Community.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPublishedImages } from '../assets/assets';
+import Loading from './Loading';
 
 const Community = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchImages = async () => {
+    setImages(dummyPublishedImages)
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+  if(loading) {
+    return (
+     <Loading />
+    )
+  }
   return (
-    <div>Community</div>
+    <div>
+
+    </div>
   )
 }
 
