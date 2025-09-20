87633cd5e1b0d155302a9d8cfe60b70d042ddc1e
kojing777 <kojingmoktan92@gmail.com>
2025-09-20 20:38:48 +0545

    Add user registration and token generation functionality in userController.js

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,27 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+
+//generate jwt token
+const generateToken = (id) => {
+        return jwt.sign({ id }, process.env.JWT_SECRET, {
+                expiresIn: '30d',
+        });
+}
+//API to register a new user
+export const registerUser = async (req, res) => {
+
+        const { name, email, password } = req.body;
+        try {
+                const userExists = await User.findOne({ email });
+                if (userExists) {
+                        return res.json({ success: false, message: "User already exists" });
+                }
+                const user = await User.create({ name, email, password });
+                const token = generateToken(user._id);
+                res.json({ success: true, token });
+        } catch (error) {
+                return res.json({ success: false, message: error.message });
+        }
+}
+
+//API to login a user
