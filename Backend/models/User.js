99e4e3ba9bbd0315e3db4ec8c8e7d9a46a27821c
kojing777 <kojingmoktan92@gmail.com>
2025-09-20 16:21:23 +0545

    Add user schema definition in User.js

---
+++ Backend/models/User.js
@@ -0,0 +1,7 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    
+})
