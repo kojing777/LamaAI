8ecaa5602149bc88af84f4afa4fccb7ec362a5e4
kojing777 <kojingmoktan92@gmail.com>
2025-09-20 23:03:46 +0545

    Add API to retrieve user details

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -42,3 +42,14 @@ export const loginUser = async (req, res) => {
                 return res.json({ success: false, message: error.message });
         }
 }
+
+//API to get user details
+export const getUser = async (req, res) => {
+        try {
+                const user = req.user;
+                return res.json({ success: true, user });
+        } catch (error) {
+                return res.json({ success: false, message: error.message });
+        }
+
+}
