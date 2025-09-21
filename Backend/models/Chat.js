0cef1fdcf945e54eef7219a660810486b5c259b5
kojing777 <kojingmoktan92@gmail.com>
2025-09-21 11:35:39 +0545

    Define Chat model schema with user and message fields

--- Backend/models/Chat.js
+++ Backend/models/Chat.js
@@ -1,3 +1,16 @@
 import mongoose from "mongoose";
 
-const chatSchema = new mongoose.Schema({});
+const chatSchema = new mongoose.Schema({
+    userId:{ type:String, required:true ,ref:"User" },
+    userName:[{ type:String, required:true }],
+    name:{type:String, required:true},
+    messages:[{
+        isImage :{ type:Boolean, required:true },
+        isPublished :{ type:Boolean, default:false },
+        role:{ type:String, required:true },
+        content:{ type:String, required:true },
+        timestamp:{ type:Date, default:Date.now }
+    }]
+},{timestamps:true});
+
+export default mongoose.model("Chat", chatSchema);
