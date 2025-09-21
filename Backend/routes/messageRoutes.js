d5483e762c07d57ccdf668cd7a1f48953a05173a
kojing777 <kojingmoktan92@gmail.com>
2025-09-21 14:43:13 +0545

    code for messageRoutes.js

--- Backend/routes/messageRoutes.js
+++ Backend/routes/messageRoutes.js
@@ -4,7 +4,7 @@ import protect from '../middlewares/auth.js'
 
 const messageRouter = express.Router();
 
-messageRouter.post('/text',protect,textMessageController)
-messageRouter.post('/image',protect,imageMessageController)
+messageRouter.post('/text', protect, textMessageController)
+messageRouter.post('/image', protect, imageMessageController)
 
 export default messageRouter
