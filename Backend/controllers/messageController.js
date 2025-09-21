7d71da4833c29d785aed2d1089ad0ebc8be801de
kojing777 <kojingmoktan92@gmail.com>
2025-09-21 14:36:23 +0545

    Implement image upload functionality in imageMessageController and adjust credit deduction logic

--- Backend/controllers/messageController.js
+++ Backend/controllers/messageController.js
@@ -1,7 +1,7 @@
+import imagekit from "../config/imagekit.js";
 import Chat from "../models/Chat.js";
 import User from "../models/User.js";
-
-
+import axios from "axios";
 
 
 //text-based ai chat maessage controller
@@ -44,12 +44,12 @@ export const textMessageController = async (req, res) => {
 export const imageMessageController = async (req, res) => {
     try {
         const userId = req.user._id;
-        if (req.user.credits <= 2) {
+        if (req.user.credits < 2) {
             return res.status(403).json({ success: false, message: "Credit pugena raja" });
         }
         const { prompt, chatId, isPublished } = req.body;
         const chat = await Chat.findOne({ userId, _id: chatId });
-      
+
         //push user message to chat
         chat.messages.push({
             isImage: false,
@@ -62,6 +62,31 @@ export const imageMessageController = async (req, res) => {
         //construct imagekit api generation url
         const generatedImageUrl = `${process.env.IMAGEKIT_URL_ENDPOINT}/ik-genimg-prompt-/${encodedPrompt}/quickgpt/${Date.now()}.png?tr=w-800,h-800`;
 
+        //trigger generation by calling the imagekit
+        const aiImageResponse = await axios.get(generatedImageUrl, { responseType: 'arraybuffer' });
+        // console.log("AI IMAGE RESPONSE", aiImageResponse.data);
+
+        //convert to base64
+        const base64Image = `data:image/png;base64,${Buffer.from(aiImageResponse.data, 'binary').toString('base64')}`;
+
+        //upload image to imagekit media library
+        const uploadResponse = await imagekit.upload({
+            file: base64Image,
+            fileName: `quickgpt/${Date.now()}.png`,
+            folder: 'quickgpt'
+        });
+        const reply = {
+            isImage: true,
+            role: "assistant",
+            timestamp: Date.now(),
+            isPublished,
+            content: uploadResponse.url
+        };
+        res.status(200).json({ success: true, reply });
+        chat.messages.push(reply);
+        await chat.save();
+
+        await User.updateOne({ _id: userId }, { $inc: { credits: -2 } });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
