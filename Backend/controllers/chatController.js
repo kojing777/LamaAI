e8c0ebe461be51812606143355fefbcbf045c498
kojing777 <kojingmoktan92@gmail.com>
2025-09-21 11:47:06 +0545

    Fix import statement for Chat model and add deleteChat API controller

--- Backend/controllers/chatController.js
+++ Backend/controllers/chatController.js
@@ -1,4 +1,4 @@
-import chat from "../models/Chat";
+import chat from "../models/Chat.js";
 
 
 
@@ -32,3 +32,14 @@ export const getChats = async (req, res) => {
 }
 
 //Api Controller for deleting a chat
+export const deleteChat = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const { chatId } = req.body;
+
+        await chat.deleteOne({ _id: chatId, userId });
+        res.json({ success: true, message: "Chat deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
