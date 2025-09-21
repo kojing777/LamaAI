0a50fde14b3b76ac6c11576a08d867b00e72c51b
kojing777 <kojingmoktan92@gmail.com>
2025-09-21 12:33:16 +0545

    Refactor message controllers to use correct model imports and add credit deduction logic; implement OpenAI chat completion for text messages

--- Backend/controllers/messageController.js
+++ Backend/controllers/messageController.js
@@ -1,4 +1,6 @@
-import chat from "../models/Chat.js";
+import Chat from "../models/Chat.js";
+import User from "../models/User.js";
+
 
 
 
@@ -9,16 +11,55 @@ export const textMessageController = async (req, res) => {
         const { chatId, prompt } = req.body;
         // Process the text message (e.g., save to database, send to AI service, etc.)
         // For demonstration, we'll just return the received message
-        const chat = await chat.findOne({ userId, _id: chatId });
+        const chat = await Chat.findOne({ userId, _id: chatId });
         chat.messages.push({
             isImage: false,
             role: "user",
             content: prompt,
             timestamp: Date.now()
         });
+        const { choices } = await openai.chat.completions.create({
+            model: "gemini-2.0-flash",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+        });
+        const reply = { ...choices[0].message, timestamp: Date.now(), isImage: false };
+        res.status(200).json({ success: true, reply });
+
+        chat.messages.push(reply);
         await chat.save();
-        res.json({ success: true, message: "Text message processed", data: { chatId, prompt } });
+
+        await User.updateOne({ _id: userId }, { $inc: { credits: -1 } });
+
     } catch (error) {
-        res.status(500).json({ success: false, message: "Internal server error", error: error.message });
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+//image-based ai chat maessage controller
+export const imageMessageController = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        if (req.user.credits <= 2) {
+            return res.status(403).json({ success: false, message: "Credit pugena raja" });
+        }
+        const { prompt, chatId, isPublished } = req.body;
+        const chat = await Chat.findOne({ userId, _id: chatId });
+      
+        //push user message to chat
+        chat.messages.push({
+            isImage: false,
+            role: "user",
+            content: prompt,
+            timestamp: Date.now()
+        });
+        
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
     }
 }
