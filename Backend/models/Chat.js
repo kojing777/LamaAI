2b7e191ce687bd247bff002c19b656811f051dac
kojing777 <kojingmoktan92@gmail.com>
2025-09-21 11:37:44 +0545

    Refactor Chat model schema for consistency and clarity

--- Backend/models/Chat.js
+++ Backend/models/Chat.js
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
 
 const chatSchema = new mongoose.Schema({
-    userId:{ type:String, required:true ,ref:"User" },
-    userName:[{ type:String, required:true }],
-    name:{type:String, required:true},
-    messages:[{
-        isImage :{ type:Boolean, required:true },
-        isPublished :{ type:Boolean, default:false },
-        role:{ type:String, required:true },
-        content:{ type:String, required:true },
-        timestamp:{ type:Date, default:Date.now }
+    userId: { type: String, required: true, ref: "User" },
+    userName: [{ type: String, required: true }],
+    name: { type: String, required: true },
+    messages: [{
+        isImage: { type: Boolean, required: true },
+        isPublished: { type: Boolean, default: false },
+        role: { type: String, required: true },
+        content: { type: String, required: true },
+        timestamp: { type: Number, required: true, default: Date.now }
     }]
-},{timestamps:true});
+}, { timestamps: true });
 
-export default mongoose.model("Chat", chatSchema);
+const chat = mongoose.model("Chat", chatSchema);
+
+export default chat;
