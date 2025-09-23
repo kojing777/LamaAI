48432e88fb14640985a475ceee2b91a917ae4bca
kojing777 <kojingmoktan92@gmail.com>
2025-09-24 00:57:38 +0545

    Fixing before deployment

--- Frontend/src/components/ChatBox.jsx
+++ Frontend/src/components/ChatBox.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useAppContext } from "../context/appContext";
+import { useAppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 import Message from "./Message";
 import toast from "react-hot-toast";
